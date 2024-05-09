0806e28bb51cc64f4a5bdac77520114711a94bc3
Jotavecorrea <161721762+Jotavecorrea@users.noreply.github.com>
2024-05-09 10:34:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function converter_temp(){
+    var celcius = prompt("Digite a temperatura em Celsius:");
+    var farenheit = celcius*1.8 + 32;
+
+    alert("A temperatura em farenheit e:" + farenheit);
+
+
+}
